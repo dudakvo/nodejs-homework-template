6bb1104fc1983c4eb2391d288bad0a5b21b8b00e
dudakvo <dudakvo@gmail.com>
2021-06-06 17:40:57 +0300

    Изменен файл server.js с использованием sequelize для подключения к БД. Созданы модели данных для таблиц БД.

--- bin/server.js
+++ bin/server.js
@@ -1,12 +1,15 @@
 const app = require("../app");
-const db = require("../model/db");
+const db = require("../models/index.js");
 
 const PORT = process.env.PORT || 3000;
 
-db.then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
+db.sequelize
+  .authenticate()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Server not runnig. Error massege: ${error.massege}`);
   });
-}).catch((error) => {
-  console.log(`Server not runnig. Error massege: ${error.massege}`);
-});
